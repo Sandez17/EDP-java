9c221c124d335103a2511cf4b169b84d779bf5e5
Sandez17 <geore1707@gmail.com>
2026-05-11 23:35:46 -0300

    Eje1

--- JavaApplication4/src/javaapplication4/Alumno.java
+++ JavaApplication4/src/javaapplication4/Alumno.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package javaapplication4;
 
-/**
- *
- * @author Yeti
- */
+
 public class Alumno {
+    int id;
+    String nombre;
+    String apellido; 
     
 }
