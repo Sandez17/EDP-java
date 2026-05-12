8a2f39c5253329361ecf8cc20883185077490050
Sandez17 <geore1707@gmail.com>
2026-05-12 00:25:52 -0300

    metodos

--- JavaApplication4/src/javaapplication4/Alumno.java
+++ JavaApplication4/src/javaapplication4/Alumno.java
@@ -7,4 +7,16 @@ public class Alumno {
     String nombre;
     String apellido; 
     
+    public void mostrarAlumno(){
+        System.out.println("Hola, soy un alumno y mi nombre es: ");
+    }
+    
+    public void saberAprobado(double calificacion){
+        if(calificacion >=6){
+            System.out.println("Aprobe");
+        }else{
+            System.out.println("No aprobo");
+        }
+    }
+    
 }
