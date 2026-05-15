968bf3acdc0594e6c1817084dada1e0d5e93ebe3
Sandez17 <geore1707@gmail.com>
2026-05-15 02:25:33 -0300

    get and set setear valores

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -1,13 +1,39 @@
-
 package javaapplication4;
-
 
 public class JavaApplication4 {
 
     public static void main(String[] args) {
         Alumno alu1 = new Alumno();
         Alumno alu2 = new Alumno(5, "Mio", "Datos");
-        System.out.println("Alumno "+alu2);
+        
+  
+        // v1 recomendable
+        System.out.println("----------------");
+        System.out.println("La id del alumno 2 es: " + alu2.getId());
+        System.out.println("El apellido del alumno 2 es: " + alu2.getApellido());
+        System.out.println("El nombre del alumno 2 es: " + alu2.getNombre());
+        // v2
+        
+        // ingresar datos
+       alu1.setId(1);
+       alu1.setNombre("Paty");
+       alu1.setApellido("Pax");
+       
+        System.out.println("----------------");
+        System.out.println("La id del alumno 2 es: " + alu1.getId());
+        System.out.println("El apellido del alumno 2 es: " + alu1.getApellido());
+        System.out.println("El nombre del alumno 2 es: " + alu1.getNombre());
+        
+        //--------Modificar datos
+        // setear cambio y valor
+        // sobre escribe el valor de un get. Esto es de la propiedad del encapsulamiento.
+        alu2.setId(34);
+        System.out.println("----------------");
+         System.out.println("La id del alumno 2 es: " + alu2.getId());
+        System.out.println("El apellido del alumno 2 es: " + alu2.getApellido());
+        System.out.println("El nombre del alumno 2 es: " + alu2.getNombre());
+        // v2
+        
     }
-    
+
 }
