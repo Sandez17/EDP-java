cdd8e1991bb67c0b9a9aecd3c5280605622f85d3
Sandez17 <geore1707@gmail.com>
2026-05-17 01:44:50 -0300

    Empleado, consultor, cargo

--- JavaApplication6/src/javaapplication6/Consultor.java
+++ JavaApplication6/src/javaapplication6/Consultor.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package javaapplication6;
 
-/**
- *
- * @author Yeti
- */
-public class Consultor {
+public class Consultor extends Persona{
+    String nombre_Consultor;
+    int num_consultor;
+
+    public Consultor() {
+    }
+
+    public Consultor(String nombre_Consultor, int num_consultor, int id, String dni, String nombre, String apellido, String domicilio, String telefono) {
+        super(id, dni, nombre, apellido, domicilio, telefono);
+        this.nombre_Consultor = nombre_Consultor;
+        this.num_consultor = num_consultor;
+    }
+
+    public void setNombre_Consultor(String nombre_Consultor) {
+        this.nombre_Consultor = nombre_Consultor;
+    }
+
+    public void setNum_consultor(int num_consultor) {
+        this.num_consultor = num_consultor;
+    }
+
+    public String getNombre_Consultor() {
+        return nombre_Consultor;
+    }
+
+    public int getNum_consultor() {
+        return num_consultor;
+    }
     
 }
--- JavaApplication6/src/javaapplication6/Empleado.java
+++ JavaApplication6/src/javaapplication6/Empleado.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package javaapplication6;
 
-/**
- *
- * @author Yeti
- */
-public class Empleado {
+
+public class Empleado extends Persona{
+    int legajoEm;
+    String cargo;
+    Double sueldo;
+
+    public Empleado() {
+    }
+
+    public Empleado(int legajoEm, String cargo, Double sueldo, int id, String dni, String nombre, String apellido, String domicilio, String telefono) {
+        super(id, dni, nombre, apellido, domicilio, telefono);
+        this.legajoEm = legajoEm;
+        this.cargo = cargo;
+        this.sueldo = sueldo;
+    }
+
+    public void setLegajoEm(int legajoEm) {
+        this.legajoEm = legajoEm;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getLegajoEm() {
+        return legajoEm;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+    
     
 }
