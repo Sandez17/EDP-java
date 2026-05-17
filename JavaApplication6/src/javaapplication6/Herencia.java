2373979f6938c36a7161eeea0b04292bf22c84ca
Sandez17 <geore1707@gmail.com>
2026-05-17 01:37:44 -0300

    Herencia

---
+++ JavaApplication6/src/javaapplication6/Herencia.java
@@ -0,0 +1,10 @@
+
+package javaapplication6;
+
+public class Herencia {
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ JavaApplication6/src/javaapplication6/Persona.java
@@ -0,0 +1,11 @@
+
+package javaapplication6;
+
+public class Persona {
+int id;
+String dni;
+String nombre;
+String apellido;
+String domicilio;
+String telefono;
+}
