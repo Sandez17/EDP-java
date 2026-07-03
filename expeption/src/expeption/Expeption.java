979508124cee4054fcc347d63c81d0f719d3131e
Sandez17 <geore1707@gmail.com>
2026-07-03 00:37:34 -0300

    exceptiones

--- expeption/src/expeption/Expeption.java
+++ expeption/src/expeption/Expeption.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package expeption;
 
-/**
- *
- * @author Yeti
- */
 public class Expeption {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // exceptiones y errores en java.
+        try {
+            int resultado = 9 / 0;
+        } catch (Exception e) {
+            System.out.println("No se puede dividir por 0. ");
+        }
+        try {
+            int edades[] = {15, 12, 23, 30};
+            System.out.println("La edad de la posicion 4 es: " + edades[4]);
+        } catch (Exception e) {
+            System.out.println("No existe, intestas a acceder a un indice que no existe. ");
+        }
     }
-    
+
 }
